cbbb2715a2bf1f2155408a63f24d1379a5905239
tellydelly <martinwilson450@gmail.com>
2022-04-29 13:48:00 -0700

    adjusting seeding script

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,7 @@
 const seed = require('./seed.json')
 const Venue = require('../models/venue-model')
 
-Venue.deleteMany({})
-.then(() => {
+
     Venue.insertMany(seed)
     .then(console.log)
     .catch(console.error)
-})
